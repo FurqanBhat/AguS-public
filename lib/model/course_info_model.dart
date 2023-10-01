c7b5110fe7881afec22d1e580f2030f946a3cf36
Furqan Bhat <furqan.eec@gmail.com>
2023-10-01 16:45:50 +0300

    Uploading without models

--- lib/model/course_info_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-class CoursesInfoModel with ChangeNotifier{
-  static int courseCode=0;
-  static int fileId=0;
-  static String fileVerifier="";
-  static String fileName="";
-}
